8274204f5d6be43e22f2bebdb32460a7a5e34c6e
machih <machih@vmware.com>
2022-06-08 16:54:49 +0900

    Add Tests

--- src/main/java/jp/vmware/sbp/kubereport/controller/spring/clients/Aggregator.java
+++ src/main/java/jp/vmware/sbp/kubereport/controller/spring/clients/Aggregator.java
@@ -1,8 +1,6 @@
 package jp.vmware.sbp.kubereport.controller.spring.clients;
 
 import jp.vmware.sbp.kubereport.controller.spring.models.V1alpha1Spreadsheet;
-import jp.vmware.sbp.kubereport.controller.spring.models.V1alpha1SpreadsheetSpec;
-import jp.vmware.sbp.kubereport.controller.spring.models.V1alpha1SpreadsheetStatusAggregated;
 
 import java.util.Map;
 
